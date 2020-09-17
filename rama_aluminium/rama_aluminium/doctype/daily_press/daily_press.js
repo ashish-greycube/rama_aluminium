936a74f83c0ea692eda11f4f10682ec2c1bec950
ashish-greycube <mr.ashish.shah@gmail.com>
2020-09-17 15:13:00 +0200

    doctypes created

---
+++ rama_aluminium/rama_aluminium/doctype/daily_press/daily_press.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, GreyCube Technologies and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Daily Press', {
+	// refresh: function(frm) {
+
+	// }
+});
