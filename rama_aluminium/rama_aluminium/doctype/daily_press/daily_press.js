601913c47a329d324965b9aedaea8794796e0ce0
ashish-greycube <mr.ashish.shah@gmail.com>
2020-09-22 09:41:33 +0530

    get data in daily press

--- rama_aluminium/rama_aluminium/doctype/daily_press/daily_press.js
+++ rama_aluminium/rama_aluminium/doctype/daily_press/daily_press.js
@@ -2,7 +2,29 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Daily Press', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		if (frm.doc.docstatus===0) {
+			frm.add_custom_button(__('Job Order'),
+				function() {
+					debugger
+					erpnext.utils.map_current_doc({
+						method: "rama_aluminium.api.make_job_order",
+						source_doctype: "Job Order CT",
+						target: frm,
+						setters: [
+							{
+								label: "Customer",
+								fieldname: "customer",
+								fieldtype: "Link",
+								options: "Customer",
+								default: frm.doc.customer || undefined
+							}
+						],
+						get_query_filters: {
+							docstatus: 0
+						}
+					})
+				}, __("Get items from"));
+		}
+	}
 });
--- rama_aluminium/rama_aluminium/doctype/daily_press/daily_press.py
+++ rama_aluminium/rama_aluminium/doctype/daily_press/daily_press.py
@@ -3,8 +3,49 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
+from erpnext.stock.doctype.item.item import get_item_defaults
+from erpnext import get_default_company
 
 class DailyPress(Document):
-	pass
+	def on_submit(self):
+		self.make_stock_entry()
+
+	def make_stock_entry(self):
+		fg_warehouse=frappe.db.get_single_value('Manufacturing Settings', 'default_fg_warehouse')
+		
+		item_defaults = get_item_defaults('1001-07',get_default_company())
+		source_warehouse=item_defaults.item_defaults[0].default_warehouse
+
+
+		work_order = frappe.get_doc("Work Order", work_order_id)
+		if not frappe.db.get_value("Warehouse", work_order.wip_warehouse, "is_group") \
+				and not work_order.skip_transfer:
+			wip_warehouse = work_order.wip_warehouse
+		else:
+			wip_warehouse = None
+
+		stock_entry = frappe.new_doc("Stock Entry")
+		stock_entry.purpose = 'Manufacture'
+		stock_entry.work_order = work_order_id
+		stock_entry.company = work_order.company
+		stock_entry.from_bom = 1
+		stock_entry.bom_no = work_order.bom_no
+		stock_entry.use_multi_level_bom = work_order.use_multi_level_bom
+		stock_entry.fg_completed_qty = qty or (flt(work_order.qty) - flt(work_order.produced_qty))
+		if work_order.bom_no:
+			stock_entry.inspection_required = frappe.db.get_value('BOM',
+				work_order.bom_no, 'inspection_required')
+
+		if purpose=="Material Transfer for Manufacture":
+			stock_entry.to_warehouse = wip_warehouse
+			stock_entry.project = work_order.project
+		else:
+			stock_entry.from_warehouse = wip_warehouse
+			stock_entry.to_warehouse = work_order.fg_warehouse
+			stock_entry.project = work_order.project
+
+		stock_entry.set_stock_entry_type()
+		stock_entry.get_items()
+		return stock_entry.as_dict()
